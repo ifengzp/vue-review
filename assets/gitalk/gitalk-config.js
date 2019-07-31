070eba14b3a155dbfec10321c5d264cdb3a35d90
Aaron <ifengzp@163.com>
2019-07-31 22:48:13 +0800

    Updates

--- assets/gitalk/gitalk-config.js
+++ assets/gitalk/gitalk-config.js
@@ -1,7 +1,7 @@
 var gitalk = new Gitalk({
   "clientID": "a181f72119b1aba79e7a",
   "clientSecret": "89b608e964480dfe417ec9076353a07e19071e57",
-  "repo": "https://ifengzp.github.io/vue-review/",
+  "repo": "https://github.com/ifengzp/vue-review.git",
   "owner": "ifengzp",
   "admin": ["ifengzp"],
   "id": window.location.pathname,
